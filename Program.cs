8504a581f281b9335945561ed5d68cb9ed2ca03c
sorganusf <54554261+sorganusf@users.noreply.github.com>
2020-02-04 20:44:35 -0500

    Commit

--- Program.cs
+++ Program.cs
@@ -4,16 +4,30 @@ namespace InClass3
 {
     class Car
     {
-        public string color = "red";
+        public string model;
+        public string color;
+        public int year;
+        public void fullThrottle()
+        {
+            Console.WriteLine("The car is going as fast as it can!");
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            Car myObj = new Car();
-            Car myObj2 = new Car();
-            Console.WriteLine(myObj.color);
-            Console.WriteLine(myObj2.color);
+            Car Ford = new Car();
+            Ford.model = "Mustang";
+            Ford.color = "red";
+            Ford.year = 1969;
+
+            Car Opel = new Car();
+            Opel.model = "Astra";
+            Opel.color = "white";
+            Opel.year = "2005";
+            
+            Console.WriteLine(Ford.model);
+            Console.WriteLine(Opel.model);
         }
     }
 }
