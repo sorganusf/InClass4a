218c4fd30fc9bbbee5bbc1ba51fdc88ac214f2ed
sorganusf <54554261+sorganusf@users.noreply.github.com>
2020-02-04 20:27:31 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace InClass3
+{
+    class Car
+    {
+        public string color = "red";
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myObj = new Car();
+            Car myObj2 = new Car();
+            Console.WriteLine(myObj.color);
+            Console.WriteLine(myObj2.color);
+        }
+    }
+}
