f7bb2bd65ac03e426fba3312c426e542fdd4456d
sorganusf <54554261+sorganusf@users.noreply.github.com>
2020-02-04 21:07:30 -0500

    Update

--- Program.cs
+++ Program.cs
@@ -4,20 +4,14 @@ namespace InClass3
 {
     class Car
     {
-        public string model;
-        public string color;
-        public int year;
-        public Car(string modelName, string modelColor, int modelYear)
-        {
-            model = modelName;
-            color = modelColor;
-            year = modelYear;
-        }
+        public string model = "Mustang";
+    }
+    class Program
+    { 
         static void Main(string[] args)
         {
-            Car Ford = new Car("Mustang", "Red", 1969);
-            Console.WriteLine(Ford.color + " " + 
-                Ford.year + " " + Ford.model);
+            Car myObj = new Car();
+            Console.WriteLine(myObj.model);
             
         }
     }
