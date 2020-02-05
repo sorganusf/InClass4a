b6b93f14790f912ad0b3265614356f948422113c
sorganusf <54554261+sorganusf@users.noreply.github.com>
2020-02-04 21:02:56 -0500

    Change

--- Program.cs
+++ Program.cs
@@ -7,27 +7,18 @@ namespace InClass3
         public string model;
         public string color;
         public int year;
-        public void fullThrottle()
+        public Car(string modelName, string modelColor, int modelYear)
         {
-            Console.WriteLine("The car is going as fast as it can!");
+            model = modelName;
+            color = modelColor;
+            year = modelYear;
         }
-    }
-    class Program
-    {
         static void Main(string[] args)
         {
-            Car Ford = new Car();
-            Ford.model = "Mustang";
-            Ford.color = "red";
-            Ford.year = 1969;
-
-            Car Opel = new Car();
-            Opel.model = "Astra";
-            Opel.color = "white";
-            Opel.year = "2005";
+            Car Ford = new Car("Mustang", "Red", 1969);
+            Console.WriteLine(Ford.color + " " + 
+                Ford.year + " " + Ford.model);
             
-            Console.WriteLine(Ford.model);
-            Console.WriteLine(Opel.model);
         }
     }
 }
